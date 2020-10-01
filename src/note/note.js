905b8e19d674840f1a40ce7eb187346f445ac677
Adrian Marquis <adrian.marquis78@gmail.com>
2020-10-01 11:47:01 -0400

    server connection

--- src/note/note.js
+++ src/note/note.js
@@ -18,6 +18,7 @@ class Note extends React.Component {
       fetch(`${Store.notes_API_ENDPOINT}/${this.props.note.id}`, {
         method: 'DELETE',
         headers: {
+          "Authorization": "Bearer 1a4d99a8-154c-406d-808c-f016b7501225",
           'content-type': 'application/json'
         },
       })
